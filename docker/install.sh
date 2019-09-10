a675a169e4ed4263a22e6ad03e62c852bb866fef
kongvut sangkla <kongvut@kkumail.com>
2019-09-10 09:28:33 +0700

    update - add seeder db

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ git pull \
 sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && mkdir -m 775 ./public/storage && chmod -R 775 ./storage && chmod -R 775 ./bootstrap/cache" \
 sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear" \
 sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && composer install --optimize-autoloader --no-dev" \
-sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate" \
+sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed" \
 sudo docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear && php artisan cache:clear && php artisan view:cache && php artisan optimize" \
 sudo docker restart mbs-php-fpm
 
