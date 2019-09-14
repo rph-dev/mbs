9bad7e1b668b2c3b3dcf560aa064132486aa6c40
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 13:15:03 +0700

    update docker bash script

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "show docker process list"
+docker ps -a
+
 echo "create mbs_web db"
 docker exec -it mbs-mariadb sh -c "mysql -uroot -proot -e 'CREATE DATABASE mbs_web CHARACTER SET \"utf8mb4\" COLLATE \"utf8mb4_unicode_ci\"'"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed"
