a4f8fd44724185e42ae54bcee82477e7bce40ed5
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 11:41:04 +0700

    update docker bash script

--- docker/create.sh
+++ docker/create.sh
@@ -12,6 +12,6 @@ echo "create mbs_web db"
 docker exec -it mbs-mariadb sh -c "mysql -uroot -proot -e 'CREATE DATABASE mbs_web CHARACTER SET \"utf8mb4\" COLLATE \"utf8mb4_unicode_ci\"'"
 
 echo "composer install"
-docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && composer install --optimize-autoloader --no-dev"
+docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && composer install"
 echo "create .env & generate key"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && cp .env.example .env && php artisan key:generate"
--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,6 @@
 git pull
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && mkdir -m 775 ./public/storage && chmod -R 777 ./storage && chmod -R 775 ./bootstrap/cache"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed"
-docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear && php artisan cache:clear && php artisan view:cache && php artisan optimize"
 docker restart mbs-php-fpm
 
 ./start.sh
