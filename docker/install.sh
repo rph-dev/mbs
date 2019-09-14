2b1a4c56fe8f9bc6150abbdbf26c12a984d3d933
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 11:42:29 +0700

    update docker bash script

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-git pull
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && mkdir -m 775 ./public/storage && chmod -R 777 ./storage && chmod -R 775 ./bootstrap/cache"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed"
 docker restart mbs-php-fpm
