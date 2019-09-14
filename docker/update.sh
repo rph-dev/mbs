9473d05460471ce4642a59297ff2cd564a2d2d32
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 11:52:34 +0700

    update docker bash script

--- docker/update.sh
+++ docker/update.sh
@@ -6,7 +6,7 @@ git pull
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && composer install"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate"
-docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear && php artisan cache:clear && && php artisan config:clear"
+docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear && php artisan cache:clear && php artisan config:clear"
 docker restart mbs-php-fpm
 
 docker exec -it -d mbs-php-fpm sh -c "php /var/www/mbs-web/artisan up"
