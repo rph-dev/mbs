da1e5d0d8a59b7597d4bf9a0c19fb17e47f6ab49
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 13:22:38 +0700

    update docker bash script

--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,7 @@
 echo "create mbs_web db"
 while ! docker exec mbs-mariadb mysqladmin --user=root --password=root --host "127.0.0.1" ping --silent &> /dev/null ; do
     echo "Waiting for database connection..."
-    sleep 2
+    sleep 5
 done
 docker exec -it mbs-mariadb sh -c "mysql -uroot -proot -e 'CREATE DATABASE mbs_web CHARACTER SET \"utf8mb4\" COLLATE \"utf8mb4_unicode_ci\"'"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed"
