83c99264963811f4c93662de75fe4aead7d616cb
kongvut sangkla <kongvut@kkumail.com>
2019-09-14 12:21:36 +0700

    update docker bash script

--- docker/create.sh
+++ docker/create.sh
@@ -8,9 +8,6 @@ docker volume create --driver local mbs_mariadb
 
 ./start.sh
 
-echo "create mbs_web db"
-docker exec -it mbs-mariadb sh -c "mysql -uroot -proot -e 'CREATE DATABASE mbs_web CHARACTER SET \"utf8mb4\" COLLATE \"utf8mb4_unicode_ci\"'"
-
 echo "composer install"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && composer install"
 echo "create .env & generate key"
--- docker/install.sh
+++ docker/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && mkdir -m 775 ./public/storage && chmod -R 777 ./storage && chmod -R 775 ./bootstrap/cache"
+echo "create mbs_web db"
+docker exec -it mbs-mariadb sh -c "mysql -e 'CREATE DATABASE mbs_web CHARACTER SET \"utf8mb4\" COLLATE \"utf8mb4_unicode_ci\"'"
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan migrate && php artisan db:seed"
+
 docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && php artisan clear && php artisan cache:clear && php artisan config:clear"
+docker exec -it mbs-php-fpm sh -c "cd /var/www/mbs-web/ && mkdir -m 775 ./public/storage && chmod -R 777 ./storage && chmod -R 775 ./bootstrap/cache"
+
 docker restart mbs-php-fpm
 
 ./start.sh
